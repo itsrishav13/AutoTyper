31de364fb4f842cfde632d2d252674f48bb83624
Rishav Kumar <113712880+itsrishav13@users.noreply.github.com>
2024-05-22 14:05:21 +0530

    Update AutoTyper.py

--- AutoTyper Project/AutoTyper.py
+++ AutoTyper Project/AutoTyper.py
@@ -1,35 +1,28 @@
 import tkinter as tk
 import pyautogui
 import time
+from pathlib import Path
 
+BASE_DIR = Path(__file__).resolve().parent
 
-#Creating window
-
+# Creating window
 root = tk.Tk()
-img = tk.PhotoImage(file="./AutoTyper Project/logo.png")
+img = tk.PhotoImage(file=BASE_DIR / "logo.png")
 root.iconphoto(False, img)
 root.geometry("720x500")
 root.title("AutoTyper App")
 
-
 # Text Entry
-
 def get_text():
-    return entry.get() # Getting Data from the first check box
+    return entry.get()  # Getting Data from the first check box
 
-
-label = tk.Label(root, text="Paste Your text here :)", font=("Helvetica", 12)) # Making Label For first entry box
+label = tk.Label(root, text="Paste Your text here :)", font=("Helvetica", 12))  # Making Label For first entry box
 label.pack()
 
-entry = tk.Entry(root, width=40, font=("Hevetica", 14))
+entry = tk.Entry(root, width=40, font=("Helvetica", 14))
 entry.pack(pady=10)
 
-
-
-
-
 # Loop
-
 def get_no_of_times():
     try:
         user_input = int(loop.get())
@@ -39,7 +32,6 @@ def get_no_of_times():
     except ValueError as e:
         error_label.config(text=str(f"{e} >> Please Enter Integer Value!"))
         return None
-    
 
 label_loop = tk.Label(root, text="Number of times?", font=("Helvetica", 12))
 label_loop.pack()
@@ -51,14 +43,7 @@ loop.insert(0, "1")  # Making default value 1
 error_label = tk.Label(root, fg="red")
 error_label.pack()
 
-
-
-
-
-
-
 # Enter Button
-
 def check_enter():
     current_state = checkbox_enter.get()
     if current_state == 1:
@@ -68,11 +53,7 @@ checkbox_enter = tk.IntVar()
 checkbox = tk.Checkbutton(root, text="Press Enter after every line?", variable=checkbox_enter, command=checkbox_enter, font=("Helvetica", 12))
 checkbox.pack(pady=10)
 
-
-
-
 # Sleep
-
 def waiting_time():
     try:
         user_input = int(wait.get())
@@ -82,19 +63,15 @@ def waiting_time():
     except ValueError as e:
         error_label.config(text=str(f"{e} >> Please Enter Integer Value! Recommended more than 3"))
         return None
-    
-label= tk.Label(root, text="Time to wait before Execution!",  font=("Helvetica", 10))
+
+label = tk.Label(root, text="Time to wait before Execution!", font=("Helvetica", 10))
 label.pack(pady=10)
-wait= tk.Entry(root, font=("Helvetica", 14))
+wait = tk.Entry(root, font=("Helvetica", 14))
 wait.pack(pady=10)
-wait.insert(0,"3")
+wait.insert(0, "3")
 
 error_label = tk.Label(root, fg="red")
 error_label.pack()
-
-
-
-
 
 # Final Result
 def paste():
@@ -105,8 +82,8 @@ def paste():
         for i in range(num_times):
             pyautogui.typewrite(f"{txt}")
             check_enter()
-button = tk.Button(root, text="Submit",font=("Helvetica", 10), command=paste)
-button.pack(pady=30)
 
+button = tk.Button(root, text="Submit", font=("Helvetica", 10), command=paste)
+button.pack(pady=30)
 
 root.mainloop()
